fd0fc4390fd89cdfab9e4bbd4238b271040c3306
darshan <127919389+darshandahal@users.noreply.github.com>
2025-10-19 20:02:15 -0600

    survey table for future response

---
+++ sqlQueries/survey-table.sql
@@ -0,0 +1,18 @@
+-- Create the survey table
+CREATE TABLE survey (
+    survey_id SERIAL PRIMARY KEY,
+    survey_name VARCHAR(255) ,
+    description TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    due_date DATE,
+    audience VARCHAR(255)
+);
+
+-- Create the questions table
+CREATE TABLE questions (
+    question_id SERIAL PRIMARY KEY,
+    survey_id INT ,
+    question_type VARCHAR(100),
+    question_title TEXT NOT NULL,
+    FOREIGN KEY (survey_id) REFERENCES survey(survey_id) ON DELETE CASCADE
+);
