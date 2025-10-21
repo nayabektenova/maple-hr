355d97b9569f75e7bc15d94389d75880244a60c4
BenSlemp <benjamin.slemp@edu.sait.ca>
2025-10-20 20:02:03 -0600

    deleted extra requests pages i missed before

--- app/requests/layout.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from "@/components/layouts/DashboardLayout";
-
-export default function RequestsLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
