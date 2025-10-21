a880226e89f3e0dd2ce467216e6780356e7c7427
BenSlemp <benjamin.slemp@edu.sait.ca>
2025-10-20 18:42:46 -0600

    removed requests page as we already have leaves

--- app/admin/requests/layout.tsx
+++
@@ -1,9 +0,0 @@
-import DashboardLayout from "@/components/layouts/DashboardLayout";
-
-export default function RequestLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return <DashboardLayout>{children}</DashboardLayout>;
-}
