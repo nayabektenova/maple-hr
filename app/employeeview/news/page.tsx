87754b86cd0a36f6c793e9d086ac21c7aa940855
BenSlemp <benjamin.slemp@edu.sait.ca>
2025-11-17 17:25:03 -0700

    created employee news page.tsx

---
+++ app/employeeview/news/page.tsx
@@ -0,0 +1,9 @@
+import EmployeeNewsFeed from "@/components/employee-news-feed";
+
+export default function Page() {
+  return (
+    <div className="p-6">
+      <EmployeeNewsFeed />
+    </div>
+  );
+}
